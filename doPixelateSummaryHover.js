144a58937055d03e3fa9aa5533f44ff0a4de1638
dianavaleryoviedo <108971155+dianavaleryoviedo@users.noreply.github.com>
2025-04-11 13:25:13 -0700

    Update doPixelateSummaryHover.js

--- doPixelateSummaryHover.js
+++ doPixelateSummaryHover.js
@@ -51,12 +51,12 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Image loaded, starting pixelation effect");
     pixelate(0.05); // Step 1: initial pixelation
     setTimeout(() => {
-        pixelate(0.06); // Step 2: final pixelation
+        pixelate(0.05); // Step 2: final pixelation
         setTimeout(() => {
             console.log("Removing canvas");
             canvas.remove();
             isAnimating = false;
-        }, 200);
+        }, 150);
     }, 0);
 };
  
